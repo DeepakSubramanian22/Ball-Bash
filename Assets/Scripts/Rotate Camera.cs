8de68b56de2f1aebda0c4eb80777e167698a4b8f
Deeapk Subramanian22 <deepakpavan0418@gmail.com>
2026-05-26 21:59:48 +0530

    Scripted-Focal Point

--- Assets/Scripts/Rotate Camera.cs
+++ Assets/Scripts/Rotate Camera.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Rotation : MonoBehaviour
 {
+    public float rotateSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +12,7 @@ public class Rotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up * (horizontal * rotateSpeed * Time.deltaTime));
     }
 }
