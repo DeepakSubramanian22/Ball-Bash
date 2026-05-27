82cbca9dcd58e946a8aa02ed75bab31d7db75617
Deeapk Subramanian22 <deepakpavan0418@gmail.com>
2026-05-27 14:54:18 +0530

    Cleaned up the code

--- Assets/Scripts/Spawn Manager.cs
+++ Assets/Scripts/Spawn Manager.cs
@@ -8,16 +8,23 @@ public class SpawnManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        float spawmx = Random.Range(-spawnPos, spawnPos);
-        float spawnZ = Random.Range(-spawnPos, spawnPos);
-        
-        Vector3 randomPos = new Vector3(spawmx, 0, spawnZ);
-        Instantiate(enemyPrefab , randomPos, Quaternion.identity);
+       
+        Instantiate(enemyPrefab , generateRandomPos(), Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
+
         
+    }
+    
+    private Vector3 generateRandomPos()
+    {
+        float spawmx = Random.Range(-spawnPos, spawnPos);
+        float spawnZ = Random.Range(-spawnPos, spawnPos);
+        
+        Vector3 randomPos = new Vector3(spawmx, 0, spawnZ);
+        return randomPos;
     }
 }
