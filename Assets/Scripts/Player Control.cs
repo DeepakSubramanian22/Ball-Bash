274eb0075ccf19d61609706884017973e36c5242
Deeapk Subramanian22 <deepakpavan0418@gmail.com>
2026-05-28 12:45:59 +0530

    added-PowerUp dash Function

--- Assets/Scripts/Player Control.cs
+++ Assets/Scripts/Player Control.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
@@ -5,6 +6,9 @@ public class PlayerControl : MonoBehaviour
     private Rigidbody PlayerRB;
     private GameObject focalPoint;
     public float speed = 5.0f;
+    public bool hasPowerUp = false;
+
+    private float powerUp = 15;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,5 +21,26 @@ public class PlayerControl : MonoBehaviour
     {
         float playerInput = Input.GetAxis("Vertical");
         PlayerRB.AddForce(focalPoint.transform.forward * (speed * playerInput));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Rigidbody enemyRb  = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 AwayfromPlayer = collision.gameObject.transform.position - transform.position;
+            
+            enemyRb.AddForce(AwayfromPlayer * powerUp , ForceMode.Impulse);
+            Debug.Log("Collided with " + collision.gameObject.name + "With the Powerup Set to" + hasPowerUp);
+        }
     }
 }
